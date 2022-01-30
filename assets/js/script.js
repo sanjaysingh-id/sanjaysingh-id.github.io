015d52497db167c0f62bfbb340fbaa45ec6a3fcd
SANJAY SINGH <55433055+sanjaysingh-id@users.noreply.github.com>
2022-01-31 00:58:56 +0700

    First Commit
    First pushing locally files into github pages

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+	// GET YOUTUBE API
+	$.ajax({
+		url: 'https://www.googleapis.com/youtube/v3/channels',
+		type: 'get',
+		dataType: 'json',
+		data: {
+			key: 'AIzaSyDX9gyh0_wx75NXuCz4i267um-2R2jPXHg',
+			part: 'snippet,statistics',
+			id: 'UCjJJmHoo8da_WOBehdBTYaQ',
+		},
+		// if success
+		success: function (result) {
+			const title = result.items[0].snippet.title;
+			const profilePic = result.items[0].snippet.thumbnails.medium.url;
+			const tsubs = result.items[0].statistics.subscriberCount;
+			const subs = tsubs.toLocaleString();
+			$('.channel-name').append(title);
+			$('.subs').append(subs, ' Subscriber');
+			$('.profilePic').attr('src', profilePic);
+		},
+	});
+});
